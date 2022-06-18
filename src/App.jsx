592da005600e735588821c06c784ac4d39cad539
Your Name <you@example.com>
2022-06-18 20:47:57 +0200

    forty commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Classes from './modules/appmod.module.css'
 import Todo from './components/todo'
-import TestCompoent from './test'
 
 function App() {
 
@@ -8,7 +7,6 @@ function App() {
     <div className={Classes.ComponentApp}>
       <>
         <Todo />
-        {/* <TestCompoent /> */}
       </>
     </div>
   )
